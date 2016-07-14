5b5670373efcf9d10890ded13874443f03223c8e
Matthew Cavanagh <maelstrome26@gmail.com>
2016-07-14 23:14:30 +0100

    Added NPM, Bower, Grunt, LESS and compilation structure

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+'use strict';
+module.exports = function(grunt) {
+    grunt.initConfig( {
+        concat: {
+            css: {
+                src: [
+                    'public/assets/compiled/less.css',
+                ],
+                dest: 'public/assets/compiled/main.css'
+            },
+            js: {
+                src: [
+                    'public/bower_components/jquery/dist/jquery.min.js'
+                ],
+                dest: 'public/assets/compiled/main.min.js'
+            }
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: true,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'public/assets/compiled/main.min.css': ['public/assets/compiled/main.css']
+                }
+            }
+        },
+        less: {
+            development: {
+                files: {
+                    'public/assets/compiled/less.css' : 'public/assets/less/main.less'
+                }
+            }
+        },
+        watch: {
+            less: {
+                files: ['public/assets/less/**/*.less'],
+                tasks: ['css'],
+                options: {
+                    nospawn: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', [
+        'css',
+        'js'
+    ]);
+
+    grunt.registerTask('css', [
+        'less',
+        'concat:css',
+        'cssmin'
+    ]);
+
+    grunt.registerTask('js', [
+        'concat:js',
+    ]);
+};
